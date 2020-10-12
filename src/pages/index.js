9db9bec86a2f5a66b43afbe4eee1bf1b963b06b3
Jeffrey Yu <jeffrey.yu@temando.com>
2020-10-12 21:36:31 +1000

    clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react"
-import { Link } from "gatsby-theme-material-ui"
 import { Container, Box, Typography } from "@material-ui/core"
 
-import ProTip from "../components/pro-tip"
 import Virtulize from "../components/search2"
 
 export default function App() {
